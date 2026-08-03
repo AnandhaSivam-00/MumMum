6ccf8e4dd34d7e3110c00c73e5fc03eac5db9617
AnandhaSivam-00 <anandsugan218@gmail.com>
2026-08-03 10:39:24 +0530

    refactor: update Loader component and remove unused loader styles and image

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,76 +1,75 @@
-import React, {useEffect} from 'react';
-import {getRestaurants, sortByRatings, sortByReviews, toggleVegOnly} from "../actions/restaurantAction";
+import React, { useEffect } from 'react';
+import { getRestaurants, sortByRatings, sortByReviews, toggleVegOnly } from "../actions/restaurantAction";
 import Restaurant from "./Restaurant";
 import Loader from "./Layout/Loader";
 import Message from "./Message";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CountRestaurant from './CountRestaurant';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const {keyword} = useParams();
-  const {
-    loading: restaurantsLoading, 
-    error: restaurantsError, 
-    restaurants,
-    showVegOnly
-  } = useSelector((state => state.restaurants));
+    const dispatch = useDispatch();
+    const { keyword } = useParams();
+    const {
+        loading: restaurantsLoading,
+        error: restaurantsError,
+        restaurants,
+        showVegOnly
+    } = useSelector((state => state.restaurants));
 
-  useEffect(() => {
-    if(restaurantsError){
-      toast.error(restaurantsError);
-      return;
-    }
-    dispatch(getRestaurants(keyword));
-  }, [dispatch, restaurantsError, keyword]);
-// {} are use to Embaded with JavaScript code with JSX code
-// restaurants is a object and restaurants.restaurants is used to access the object
+    useEffect(() => {
+        if (restaurantsError) {
+            toast.error(restaurantsError);
+            return;
+        }
+        dispatch(getRestaurants(keyword));
+    }, [dispatch, restaurantsError, keyword]);
+    // {} are use to Embaded with JavaScript code with JSX code
+    // restaurants is a object and restaurants.restaurants is used to access the object
 
-const handleSortByRatings = () => {
-  dispatch(sortByRatings());
-};
+    const handleSortByRatings = () => {
+        dispatch(sortByRatings());
+    };
 
-const handleSortByReviews = () => {
-  dispatch(sortByReviews());
-};
+    const handleSortByReviews = () => {
+        dispatch(sortByReviews());
+    };
 
-const handleToggleVegOnly = () => {
-  dispatch(toggleVegOnly());
-};
+    const handleToggleVegOnly = () => {
+        dispatch(toggleVegOnly());
+    };
 
-  return (
-    <>
-      <CountRestaurant />
-      {restaurantsLoading ? (
-      <Loader/>) : restaurantsError ? (
-      <Message variant="danger">{restaurantsError}</Message>) : (
-        <>  
-        <section>
-          <div className="sort">
-            <button className="sort_veg p-2" onClick={handleToggleVegOnly}>
-              {showVegOnly ? "Show All" : "Pure Veg"}
-            </button>
-            <button className="sort_rev p-2" onClick={handleSortByReviews}>Sort By Reviews</button>
-            <button className="sort_rate p-2" onClick={handleSortByRatings}>Sort By Ratings</button>
-          </div>
-          <div className="row mt-4">  
-              {restaurants && restaurants.restaurants ? (
-                restaurants.restaurants.map((restaurant) => !showVegOnly || (showVegOnly && restaurant.isVeg) ? (
-                  <Restaurant key={restaurant._id} restaurant={restaurant} />
-                ) : null )
-                ) : (
-                  <Message variant="info"><span id="found">No Restaurants Found </span></Message>
-                )}
-          </div>
-        </section>
-        {/* <ScrollBack /> */}
+    return (
+        <>
+            <CountRestaurant />
+            {restaurantsLoading ? (
+                <Loader />) : restaurantsError ? (
+                    <Message variant="danger">{restaurantsError}</Message>) : (
+                <>
+                    <section>
+                        <div className="sort">
+                            <button className="sort_veg p-2" onClick={handleToggleVegOnly}>
+                                {showVegOnly ? "Show All" : "Pure Veg"}
+                            </button>
+                            <button className="sort_rev p-2" onClick={handleSortByReviews}>Sort By Reviews</button>
+                            <button className="sort_rate p-2" onClick={handleSortByRatings}>Sort By Ratings</button>
+                        </div>
+                        <div className="row mt-4">
+                            {restaurants && restaurants.restaurants ? (
+                                restaurants.restaurants.map((restaurant) => !showVegOnly || (showVegOnly && restaurant.isVeg) ? (
+                                    <Restaurant key={restaurant._id} restaurant={restaurant} />
+                                ) : null)
+                            ) : (
+                                <Message variant="info"><span id="found">No Restaurants Found </span></Message>
+                            )}
+                        </div>
+                    </section>
+                </>
+            )}
         </>
-         )}
-    </>
-  )
+    )
 }
 
 export default Home
--- frontend/src/components/Layout/Loader.jsx
+++ frontend/src/components/Layout/Loader.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
-import loaderImage from '../../Images/system-update.png'
-{/* <span className="loading-text">Loading...</span> */}
+
 const Loader = () => {
-  return (
-    <div className="loader">
-      <img src={loaderImage} alt="loading..." className="loading-icon" />
-      <div className="loading-text-select">
-        <span>L</span>
-        <span>o</span>
-        <span>a</span>
-        <span>d</span>
-        <span>i</span>
-        <span>n</span>
-        <span>g</span>
-        <span>.</span>
-      </div>
-    </div>
-  )
+    return (
+        <div className="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+            <div 
+                className="spinner-border mb-3" 
+                role="status"
+                style={{
+                    color: 'var(--bronze-hover)'
+                }}
+            >
+                <span className="visually-hidden" style={{ visibility: 'hidden' }}>Loading...</span>
+            </div>
+            <div className="loading-text-select">
+                <span>L</span>
+                <span>o</span>
+                <span>a</span>
+                <span>d</span>
+                <span>i</span>
+                <span>n</span>
+                <span>g</span>
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+            </div>
+        </div>
+    )
 }
 
 export default Loader
